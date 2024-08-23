a00f61ef5bdad56e37cda54eb31fe5d8253d39ab
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2024-08-23 21:41:08 +0000

    added objects

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,22 +1,55 @@
 
-
+// select elements by query selector;
 const tile = document.querySelectorAll('.tile');
-   let turn = true;
-   console.log(typeof turn)
-tile.forEach((tile)=>{
-    console.log(typeof tile)
-    tile.addEventListener("click",(button)=>{ 
-        if(turn === true){  
-            console.log(butto)
 
-            turn = !turn;  
+function tileStat() {
+   this.isChecked = false;
+   this.isCircle = false;
+   this.position = 0;
  }
- else{  
-    console.log("heelo")
-    //tile.innerHTML(`<img src="./resources/circle-svgrepo-com.svg" alt="circle">`);
-    //console.log(turn);
-    turn = !turn;   
- }
-    })
- 
+let i = 1;
+tile.forEach((e)=>{
+   const Stat = new tileStat();
+   Stat.position = i;
+   i++;
+   console.log(Stat);
+   e.addEventListener('click',()=>{
+      if(e.Stat.isChecked===false){
+         console.log(hello)
+      }
+   })
+
 })
+
+
+// function that add elements on click 
+function addSVG(isCircle){
+   if(isCircle){
+      // circle added in dom
+   }
+   else {
+      // cross added in dom
+   }
+}
+
+//funtion that checks if there are elements in a line
+
+function checkPanel(PanelArray){
+  //checks the array.
+
+  /*
+  123
+  456
+  789
+  147
+  258
+  369
+  357
+  159
+  */
+}
+
+// win/loose and playAgain alert 
+function finalMessage(){
+
+}
